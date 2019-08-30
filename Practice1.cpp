dd869b590ed6ca0651eb5bac505806cfde88bfd7
Leroy Hatcher <hatcherl@vcu.edu>
2019-08-29 21:03:41 -0400

    Commit2

--- Practice1.cpp
+++ Practice1.cpp
@@ -3,7 +3,16 @@ using namespace std;
 
 int main()
 {
-	cin.get();
+	int value = 1; /* Must have initialized value before using in this while statement*/
+	while (value != 0)
+	{
+		cout << "Please give me a value: " << endl;
+		cin >> value;
+		if (value == 0)
+			break;
+		cout << "The square of your value is " << pow(value, 2) << endl;
+	}
 
+	system("pause");
 	return 0;
 }
